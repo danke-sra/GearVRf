1ee2fc106bc5062d13a165342ebf8ff3017379e1
Danke Xie <danke.xie@gmail.com>
2016-01-25 13:10:32 -0800

    DebugServer: Support line editing and history
    Work in progress

--- GVRf/Framework/src/org/gearvrf/debug/ConsoleFactory.java
+++ GVRf/Framework/src/org/gearvrf/debug/ConsoleFactory.java
@@ -16,9 +16,16 @@
 package org.gearvrf.debug;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
+
+import jline.ConsoleReader;
 
 import org.gearvrf.debug.cli.CommandTable;
 import org.gearvrf.debug.cli.ConsoleIO;
@@ -28,6 +35,10 @@ import org.gearvrf.debug.cli.Shell;
 import org.gearvrf.debug.cli.util.ArrayHashMultiMap;
 import org.gearvrf.debug.cli.util.MultiMap;
 
+/**
+ * This class contains factory methods for creating consoles of different
+ * capabilities.
+ */
 class ConsoleFactory {
     /**
      * Facade method for operating the Shell allowing specification of auxiliary
@@ -37,10 +48,13 @@ class ConsoleFactory {
      *
      * @see org.gearvrf.debug.cli.Shell#Shell(org.gearvrf.debug.cli.Shell.Settings, org.gearvrf.debug.cli.CommandTable, java.util.List)
      *
-     * @param prompt Prompt to be displayed
-     * @param appName The app name string
-     * @param mainHandler Main command handler
-     * @param auxHandlers Aux handlers to be passed to all subshells.
+     * @param prompt Prompt to be displayed.
+     * @param appName The app name string.
+     * @param mainHandler Main command handler.
+     * @param in Input reader.
+     * @param out Output stream.
+     * @param err Error output stream.
+     *
      * @return Shell that can be either further customized or run directly by calling commandLoop().
      */
     static Shell createConsoleShell(String prompt, String appName, Object mainHandler,
@@ -62,5 +76,145 @@ class ConsoleFactory {
         theShell.addMainHandler(mainHandler, "");
 
         return theShell;
+    }
+
+    /**
+     * Facade method for operating the Unix-like Shell supporting line editing and command
+     * history.
+     *
+     * @param prompt Prompt to be displayed
+     * @param appName The app name string
+     * @param mainHandler Main command handler
+     * @param input Input stream.
+     * @param output Output stream.
+     * @return Shell that can be either further customized or run directly by calling commandLoop().
+     */
+    static Shell createUnixConsoleShell(String prompt, String appName,
+            ShellCommandHandler mainHandler, InputStream input, OutputStream output,
+            InputStream jlineKeyBindings) {
+        try {
+            // Build jline terminal
+            PrintStream out = new PrintStream(output);
+
+            jline.Terminal term = jline.Terminal.setupTerminal();
+
+            ConsoleReader console = new ConsoleReader(input, new PrintWriter(out), jlineKeyBindings, term);
+            console.setBellEnabled(true);
+
+            // Build console
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    new ConsoleReaderInputStream(console)));
+
+            return createConsoleShell(prompt, appName, mainHandler, in, out, out);
+        } catch (IOException e) {
+            // Failover: use default shell
+            BufferedReader in = new BufferedReader(new InputStreamReader(input));
+            PrintStream out = new PrintStream(output);
+
+            return createConsoleShell(prompt, appName, mainHandler, in, out, out);
+        }
+    }
+
+    /**
+     * Facade method for operating the Telnet Shell supporting line editing and command
+     * history over a socket.
+     *
+     * @param prompt Prompt to be displayed
+     * @param appName The app name string
+     * @param mainHandler Main command handler
+     * @param input Input stream.
+     * @param output Output stream.
+     * @return Shell that can be either further customized or run directly by calling commandLoop().
+     */
+    static Shell createTelnetConsoleShell(String prompt, String appName,
+            ShellCommandHandler mainHandler, InputStream input, OutputStream output,
+            InputStream jlineKeyBindings) {
+        try {
+            // Set up nvt4j; ignore the initial clear & reposition
+            final nvt4j.impl.Terminal nvt4jTerminal = new nvt4j.impl.Terminal(input, output) {
+                private boolean cleared;
+                private boolean moved;
+
+                @Override
+                public void clear() throws IOException {
+                    if (this.cleared)
+                        super.clear();
+                    this.cleared = true;
+                }
+
+                @Override
+                public void move(int row, int col) throws IOException {
+                    if (this.moved)
+                        super.move(row, col);
+                    this.moved = true;
+                }
+            };
+            nvt4jTerminal.put(nvt4j.impl.Terminal.AUTO_WRAP_ON);
+            nvt4jTerminal.setCursor(true);
+
+            // Have JLine do input & output through telnet terminal
+            final InputStream jlineInput = new InputStream() {
+                @Override
+                public int read() throws IOException {
+                    return nvt4jTerminal.get();
+                }
+            };
+            final OutputStream jlineOutput = new OutputStream() {
+                @Override
+                public void write(int value) throws IOException {
+                    nvt4jTerminal.put(value);
+                }
+            };
+
+            return createUnixConsoleShell(prompt, appName, mainHandler, jlineInput, jlineOutput, jlineKeyBindings);
+        } catch (IOException e) {
+            // Failover: use default shell
+            BufferedReader in = new BufferedReader(new InputStreamReader(input));
+            PrintStream out = new PrintStream(output);
+
+            return createConsoleShell(prompt, appName, mainHandler, in, out, out);
+        }
+    }
+}
+
+/**
+ * InputStream wrapper for jline.ConsoleReader
+ */
+class ConsoleReaderInputStream extends InputStream {
+    private final ConsoleReader reader;
+    private String line = null;
+    private int index = 0;
+    private boolean eol = false;
+
+    public ConsoleReaderInputStream(final ConsoleReader reader) {
+        this.reader = reader;
+    }
+
+    public int read() throws IOException {
+        if (eol) {
+            eol = false;
+            return -1; // (temporary) end of stream
+        }
+
+        if (line == null) {
+            try {
+                line = reader.readLine();
+            } catch (IOException e) {
+                return -1;
+            }
+        }
+
+        if (line == null) {
+            return -1;
+        }
+
+        if (index >= line.length()) {
+            index = 0;
+            line = null;
+            eol = true;
+            return '\n'; // lines are ended with a newline
+        }
+
+        return line.charAt(index++);
     }
 }
--- GVRf/Framework/src/org/gearvrf/debug/DebugServer.java
+++ GVRf/Framework/src/org/gearvrf/debug/DebugServer.java
@@ -15,7 +15,9 @@
 
 package org.gearvrf.debug;
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.ServerSocket;
@@ -43,9 +45,9 @@ import org.gearvrf.debug.cli.Shell;
  * the terminal to add a CR (\r) for each LF (\n) for proper display.
  */
 public class DebugServer implements Runnable {
-    public static final boolean RUN_DEBUG_SERVER = true;
     public static final int DEFAULT_DEBUG_PORT = 1645;
     public static final int NUM_CLIENTS = 2;
+    public static final boolean SIMULATE_TELNET = true;
 
     private static final String PROMPT = "gvrf";
     private static final String APP_NAME = "GearVR Framework";
@@ -67,12 +69,26 @@ public class DebugServer implements Runnable {
 
         @Override
         public Object call() throws Exception {
-            PrintStream out = new PrintStream(socket.getOutputStream());
             // Hand over to shell
-            Shell shell = ConsoleFactory.createConsoleShell(PROMPT, APP_NAME,
-                    new ShellCommandHandler(gvrContext),
-                    new BufferedReader(new InputStreamReader(socket.getInputStream())),
-                    out, out);
+            Shell shell = null;
+
+            if (SIMULATE_TELNET) {
+                InputStream keyBindings = new ByteArrayInputStream(
+                        JlineKeyBindings.KeyBindings_getBlock());
+
+                // Supporting editing and history
+                shell = ConsoleFactory.createTelnetConsoleShell(PROMPT, APP_NAME,
+                        new ShellCommandHandler(gvrContext),
+                        socket.getInputStream(), socket.getOutputStream(), keyBindings);
+            } else {
+                // Simple console
+                PrintStream out = new PrintStream(socket.getOutputStream());
+                shell = ConsoleFactory.createConsoleShell(PROMPT, APP_NAME,
+                        new ShellCommandHandler(gvrContext),
+                        new BufferedReader(new InputStreamReader(socket.getInputStream())),
+                        out, out);
+            }
+
             shell.commandLoop();
             socket.close();
             return null;
